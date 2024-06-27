73f4f071b569cdc3ab58942bf35f78c82eab7747
UmangJ3 <haharofl14@gmail.com>
2024-06-28 00:28:51 +0530

    Line Reduction in checker.c

--- checker.c
+++ checker.c
@@ -1,8 +1,7 @@
 #include <assert.h>
 #include "BMS.h"
 
-void testBatteryStatus() 
-{
+void testBatteryStatus() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0.0));
   assert(!batteryIsOk(-1, 50, 0.5));
@@ -10,8 +9,7 @@ void testBatteryStatus()
   assert(!batteryIsOk(25, 50, 0.9));
 }
 
-int main() 
-{
+int main() {
   testBatteryStatus();
   return 0;
 }
