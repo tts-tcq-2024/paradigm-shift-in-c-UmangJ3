dae0c7ebeb6de146fea008c0f2a1901e054b7abc
UmangJ3 <haharofl14@gmail.com>
2024-06-28 00:07:46 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -8,8 +8,6 @@ void testBatteryStatus()
   assert(!batteryIsOk(-1, 50, 0.5));
   assert(!batteryIsOk(25, 19, 0.5));
   assert(!batteryIsOk(25, 50, 0.9));
-  assert(batteryIsOk(0, 20, 0.8));
-  assert(batteryIsOk(45, 80, 0.8));
 }
 
 int main() 
