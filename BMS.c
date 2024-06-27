db14caf9f773b638230dffa67a446685bd34fcd8
UmangJ3 <haharofl14@gmail.com>
2024-06-28 00:27:25 +0530

    Line reduction in BMS.c

--- BMS.c
+++ BMS.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include "BMS.h"
 
-const char* getStatusMessage(BatteryStatus status) 
-{
-  switch (status) 
-  {
+const char* getStatusMessage(BatteryStatus status) {
+  switch (status) {
     case TEMPERATURE_LOW: return "Temperature too low!";
     case TEMPERATURE_HIGH: return "Temperature too high!";
     case SOC_LOW: return "State of Charge too low!";
@@ -14,40 +12,32 @@ const char* getStatusMessage(BatteryStatus status)
   }
 }
 
-BatteryStatus evaluateBattery(float temperature, float soc, float chargeRate) 
-{
-  if (isTemperatureOutOfRange(temperature)) 
-  {
+BatteryStatus evaluateBattery(float temperature, float soc, float chargeRate) {
+  if (isTemperatureOutOfRange(temperature)) {
     return TEMPERATURE_LOW;
   }
-  if (isSocOutOfRange(soc)) 
-  {
+  if (isSocOutOfRange(soc)) {
     return SOC_LOW;
   }
-  if (isChargeRateOutOfRange(chargeRate)) 
-  {
+  if (isChargeRateOutOfRange(chargeRate)) {
     return CHARGE_RATE_HIGH;
   }
   return OK;
 }
 
-int isTemperatureOutOfRange(float temperature) 
-{
+int isTemperatureOutOfRange(float temperature) {
   return (temperature < 0 || temperature > 45);
 }
 
-int isSocOutOfRange(float soc) 
-{
+int isSocOutOfRange(float soc) {
   return (soc < 20 || soc > 80);
 }
 
-int isChargeRateOutOfRange(float chargeRate) 
-{
+int isChargeRateOutOfRange(float chargeRate) {
   return (chargeRate > 0.8);
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
+int batteryIsOk(float temperature, float soc, float chargeRate) {
   BatteryStatus status = evaluateBattery(temperature, soc, chargeRate);
   printf("%s\n", getStatusMessage(status));
   return status == OK;
