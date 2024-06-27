d8ac5b88b06c0d64d0e292818d3577ea997c82ac
UmangJ3 <haharofl14@gmail.com>
2024-06-27 23:38:04 +0530

    Header file for BMS.C

---
+++ BMS.h
@@ -0,0 +1,18 @@
+#ifndef BMS_H
+#define BMS_H
+
+typedef enum 
+{
+  OK,
+  TEMPERATURE_LOW,
+  TEMPERATURE_HIGH,
+  SOC_LOW,
+  SOC_HIGH,
+  CHARGE_RATE_HIGH
+} BatteryStatus;
+
+const char* getStatusMessage(BatteryStatus status);
+BatteryStatus evaluateBattery(float temperature, float soc, float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate);
+
+#endif // BMS_H
