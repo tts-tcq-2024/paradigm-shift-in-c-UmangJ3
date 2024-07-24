2419ee30475b32824c8f0036bb014adc248fdb80
UmangJ3 <haharofl14@gmail.com>
2024-07-24 10:11:06 +0530

    Update in checker.c

--- checker.c
+++ checker.c
@@ -1,12 +1,8 @@
 #include <assert.h>
 #include "BMS.h"
 
-void testBatteryStatus() {
+int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0.0));
-}
-
-int main() {
-  testBatteryStatus();
   return 0;
 }
