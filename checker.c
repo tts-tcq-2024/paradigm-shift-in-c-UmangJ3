595dddb6eb77ed711c4235dc02a8dff6570a3d7a
UmangJ3 <haharofl14@gmail.com>
2024-07-24 12:51:22 +0530

    Update in checker.c

--- checker.c
+++ checker.c
@@ -1,77 +1,23 @@
-#include <assert.h>
 #include <stdio.h>
+#include <assert.h>
 
-typedef enum {
-  OK,
-  TEMPERATURE_LOW,
-  TEMPERATURE_HIGH,
-  SOC_LOW,
-  SOC_HIGH,
-  CHARGE_RATE_HIGH
-} BatteryStatus;
-
-
-const char* getStatusMessage(BatteryStatus status) {
-    static const char* statusMessages[] = {
-        [TEMPERATURE_LOW] = "Temperature too low!",
-        [TEMPERATURE_HIGH] = "Temperature too high!",
-        [SOC_LOW] = "State of Charge too low!",
-        [SOC_HIGH] = "State of Charge too high!",
-        [CHARGE_RATE_HIGH] = "Charge Rate too high!",
-        [OK] = "Battery is OK."
-    };
-
-    if (status < 0 || status >= sizeof(statusMessages) / sizeof(statusMessages[0])) {
-        return "Unknown status";
-    }
-
-    return statusMessages[status];
+int checkTemperature(float temperature) {
+    return !(temperature < 0 || temperature > 45);
 }
 
-BatteryStatus evaluateTemperature(float temperature) {
-    if (temperature < 0) {
-        return TEMPERATURE_LOW;
-    } else if (temperature > 45) {
-        return TEMPERATURE_HIGH;
-    }
-    return OK;
+int checkSoc(float soc) {
+    return !(soc < 20 || soc > 80);
 }
 
-BatteryStatus evaluateSoc(float soc) {
-    if (soc < 20) {
-        return SOC_LOW;
-    } else if (soc > 80) {
-        return SOC_HIGH;
-    }
-    return OK;
-}
-
-BatteryStatus evaluateChargeRate(float chargeRate) {
-    if (chargeRate > 0.8) {
-        return CHARGE_RATE_HIGH;
-    }
-    return OK;
-}
-
-int isTemperatureOk(float temperature) {
-    return evaluateTemperature(temperature) == OK;
-}
-
-int isSocOk(float soc) {
-    return evaluateSoc(soc) == OK;
-}
-
-int isChargeRateOk(float chargeRate) {
-    return evaluateChargeRate(chargeRate) == OK;
+int checkChargeRate(float chargeRate) {
+    return !(chargeRate > 0.8);
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
+    return checkTemperature(temperature) && checkSoc(soc) && checkChargeRate(chargeRate);
 }
 
-
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0.0));
-  return 0;
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
 }
